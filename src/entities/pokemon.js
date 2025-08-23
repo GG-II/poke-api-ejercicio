57b6761395620596704ce6c34e9ffbafa1e20f28
JeissonGAvila <124946416+JeissonGAvila@users.noreply.github.com>
2025-08-23 11:02:27 -0600

    feat: integración completa de API Pokemon con búsqueda funcional

---
+++ src/entities/pokemon.js
@@ -0,0 +1,50 @@
+/*
+ * Pokemon Entity
+ * @author: [Su Nombre Completo]
+ * @email: [su.email@ejemplo.com]
+ * @description: Modelo de datos y validaciones para Pokemon
+ * @date: [Fecha de hoy]
+ */
+
+class Pokemon {
+    constructor(data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.height = data.height;
+        this.weight = data.weight;
+        this.types = data.types;
+        this.sprites = data.sprites;
+        this.stats = data.stats;
+    }
+
+    getFormattedHeight() {
+        return (this.height / 10).toFixed(1) + ' m';
+    }
+
+    getFormattedWeight() {
+        return (this.weight / 10).toFixed(1) + ' kg';
+    }
+
+    getTypeNames() {
+        return this.types.map(type => type.type.name);
+    }
+
+    getMainImage() {
+        return this.sprites.other['official-artwork'].front_default || 
+               this.sprites.front_default;
+    }
+
+    getStatValue(statName) {
+        const stat = this.stats.find(s => s.stat.name === statName);
+        return stat ? stat.base_stat : 0;
+    }
+}
+
+function validatePokemonData(data) {
+    return data && data.name && data.id && data.sprites;
+}
+
+// Para usar en otros archivos
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { Pokemon, validatePokemonData };
+}
